ba9466e26e29e68f0cbb916c6749c5d51c8544f6
jhohan alexander vasquez <jhohan25@hotmail.com>
2019-01-29 00:44:41 -0500

    Final

--- ConsoleUNC/Program.cs
+++ ConsoleUNC/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
 
@@ -11,103 +13,53 @@ namespace ConsoleUNC
 
         static void Main(string[] args)
         {
-            var networkPath = @"\\{Your IP or Folder Name of Network}\Shared Data";
-            NetworkCredential credentials = new NetworkCredential(@"{User Name}", "{Password}");
-            var result = ConnectToSharedFolder(networkPath, credentials);
-            if (result != 0)
-            {
-                throw new Win32Exception(result, "Error connecting to remote share");
-            }
+            var filePath = @"\\DESKTOP-T6QH60F\Prueba2\file.txt";
+            var savePath = @"C:\\temp\file.txt";
+            SaveACopyfileToServer(filePath, savePath);
         }
 
 
-        static int ConnectToSharedFolder(string networkName, NetworkCredential credentials)
+        public static void SaveACopyfileToServer(string filePath, string savePath)
         {
-            _networkName = networkName;
+            var directory = Path.GetDirectoryName(savePath).Trim();
+            var username = @"Jhohan";
+            var password = "santiago0520";
+            var filenameToSave = Path.GetFileName(savePath);
 
-            var netResource = new NetResource
+            if (!directory.EndsWith("\\"))
+                filenameToSave = "\\" + filenameToSave;
+
+            var command = "NET USE " + directory;
+            ExecuteCommand(command, 5000);
+
+            command = "NET USE " + directory + " /user:" + username + " " + password;
+            ExecuteCommand(command, 5000);
+
+            command = " copy \"" + filePath + "\"  \"" + directory + filenameToSave + "\"";
+
+            ExecuteCommand(command, 5000);
+
+
+            command = "NET USE " + directory;
+            ExecuteCommand(command, 5000);
+        }
+
+        public static int ExecuteCommand(string command, int timeout)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/C " + command)
             {
-                Scope = ResourceScope.GlobalNetwork,
-                ResourceType = ResourceType.Disk,
-                DisplayType = ResourceDisplaytype.Share,
-                RemoteName = networkName
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                WorkingDirectory = "C:\\",
             };
 
-            var userName = string.IsNullOrEmpty(credentials.Domain)
-                ? credentials.UserName
-                : string.Format(@"{0}\{1}", credentials.Domain, credentials.UserName);
-
-            return WNetAddConnection2(
-                netResource,
-                credentials.Password,
-                userName,
-                0);
+            var process = Process.Start(processInfo);
+            process.WaitForExit(timeout);
+            var exitCode = process.ExitCode;
+            process.Close();
+            return exitCode;
         }
 
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            WNetCancelConnection2(_networkName, 0, true);
-        }
-
-        [DllImport("mpr.dll")]
-        private static extern int WNetAddConnection2(NetResource netResource,
-            string password, string username, int flags);
-
-        [DllImport("mpr.dll")]
-        private static extern int WNetCancelConnection2(string name, int flags,
-            bool force);
-
-        [StructLayout(LayoutKind.Sequential)]
-        public class NetResource
-        {
-            public ResourceScope Scope;
-            public ResourceType ResourceType;
-            public ResourceDisplaytype DisplayType;
-            public int Usage;
-            public string LocalName;
-            public string RemoteName;
-            public string Comment;
-            public string Provider;
-        }
-
-        public enum ResourceScope : int
-        {
-            Connected = 1,
-            GlobalNetwork,
-            Remembered,
-            Recent,
-            Context
-        };
-
-        public enum ResourceType : int
-        {
-            Any = 0,
-            Disk = 1,
-            Print = 2,
-            Reserved = 8,
-        }
-
-        public enum ResourceDisplaytype : int
-        {
-            Generic = 0x0,
-            Domain = 0x01,
-            Server = 0x02,
-            Share = 0x03,
-            File = 0x04,
-            Group = 0x05,
-            Network = 0x06,
-            Root = 0x07,
-            Shareadmin = 0x08,
-            Directory = 0x09,
-            Tree = 0x0a,
-            Ndscontainer = 0x0b
-        }
     }
 }
